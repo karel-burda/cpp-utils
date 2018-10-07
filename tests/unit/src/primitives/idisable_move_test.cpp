544827236322f86b12dc9088e76940ba79ff0555
Karel Burda <karel.burda@nxp.com>
2018-10-07 20:46:57 +0200

    Updated tests

--- tests/unit/src/primitives/idisable_move_test.cpp
+++ tests/unit/src/primitives/idisable_move_test.cpp
@@ -4,8 +4,6 @@
 #include <test_utils/test_utils.hpp>
 #include <test_utils/static_class_assertions.hpp>
 
-// TODO: move this 2 tests into the template
-
 namespace
 {
 namespace primitives = burda::cpp_utils::primitives;
@@ -15,18 +13,18 @@ struct child_of_move : private primitives::idisable_move
 {
 };
 
-TEST(idisable_copy, static_assertions)
+TEST(idisable_move, static_assertions)
 {
     test_utils::assert_default_constructibility<primitives::idisable_move, true>();
-    test_utils::assert_copy_constructibility<primitives::idisable_move, false>();
-    test_utils::assert_move_constructibility<primitives::idisable_move, true>();
+    test_utils::assert_copy_constructibility<primitives::idisable_move, true>();
+    test_utils::assert_move_constructibility<primitives::idisable_move, false>();
 
     test_utils::assert_default_constructibility<child_of_move, true>();
-    test_utils::assert_copy_constructibility<child_of_move, false>();
-    test_utils::assert_move_constructibility<child_of_move, true>();
+    test_utils::assert_copy_constructibility<child_of_move, true>();
+    test_utils::assert_move_constructibility<child_of_move, false>();
 }
 
-TEST(idisable_copy, construction_destruction)
+TEST(idisable_move, construction_destruction)
 {
     test_utils::assert_construction_and_destruction<child_of_move>();
 }
