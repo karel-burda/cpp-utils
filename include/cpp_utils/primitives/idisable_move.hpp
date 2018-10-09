554817bbebc96812dd5da8975eea96a793224332
Karel Burda <karel.burda@nxp.com>
2018-10-09 20:14:19 +0200

    Updated documentation

--- include/cpp_utils/primitives/idisable_move.hpp
+++ include/cpp_utils/primitives/idisable_move.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 /// Origin: https://github.com/karel-burda/cpp-utils
-/// Version: 1.1.0
+/// Version: 1.1.1
 
 namespace burda
 {
@@ -10,6 +10,8 @@ namespace cpp_utils
 namespace primitives
 {
 /// Helper class that enables default construction and disables move operations
+/// Note that even this class is actually std::move_constructible, because the r-value might be transformed
+/// to the l-value and copy construction might be made.
 /// Recommended way of inheriting from this base class is private
 struct idisable_move
 {
