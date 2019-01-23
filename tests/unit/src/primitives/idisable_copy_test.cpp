72cde252414e142613f21a07cbb926f443915b0a
Karel Burda <karel.burda@nxp.com>
2019-01-23 18:50:18 +0100

    Reflecting updated API of test-utils

--- tests/unit/src/primitives/idisable_copy_test.cpp
+++ tests/unit/src/primitives/idisable_copy_test.cpp
@@ -1,8 +1,8 @@
 #include <gtest/gtest.h>
 
 #include <cpp_utils/primitives/idisable_copy.hpp>
-#include <test_utils/lifetime_assertions.hpp>
-#include <test_utils/static_class_assertions.hpp>
+#include <test_utils/lifetime.hpp>
+#include <test_utils/statics.hpp>
 
 namespace
 {
@@ -15,17 +15,17 @@ struct child_of_copy : private primitives::idisable_copy
 
 TEST(idisable_copy, static_assertions)
 {
-    test_utils::assert_default_constructibility<primitives::idisable_copy, true>();
-    test_utils::assert_copy_constructibility<primitives::idisable_copy, false>();
-    test_utils::assert_move_constructibility<primitives::idisable_copy, true>();
+    test_utils::statics::assert_default_constructibility<primitives::idisable_copy, true>();
+    test_utils::statics::assert_copy_constructibility<primitives::idisable_copy, false>();
+    test_utils::statics::assert_move_constructibility<primitives::idisable_copy, true>();
 
-    test_utils::assert_default_constructibility<child_of_copy, true>();
-    test_utils::assert_copy_constructibility<child_of_copy, false>();
-    test_utils::assert_move_constructibility<child_of_copy, true>();
+    test_utils::statics::assert_default_constructibility<child_of_copy, true>();
+    test_utils::statics::assert_copy_constructibility<child_of_copy, false>();
+    test_utils::statics::assert_move_constructibility<child_of_copy, true>();
 }
 
 TEST(idisable_copy, construction_destruction)
 {
-    test_utils::assert_construction_and_destruction<child_of_copy>();
+    test_utils::lifetime::assert_construction_and_destruction<child_of_copy>();
 }
 }
